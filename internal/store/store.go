b4a4b957beab39ea4dc13ffe03c21ef9db8c4405
Ray Zhao <ray040406@gmail.com>
2026-03-20 14:42:34 +1300

    crud implementation

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,5 +1,9 @@
 package store
 
+import (
+	"sync"
+)
+
 // TODO: 实现内存 KV 存储
 //
 // 你需要做的：
@@ -14,3 +18,49 @@ package store
 // 需要的包：
 //   - "sync"
 //   - "time"（如果做 TTL）
+
+type Store struct {
+	mu   sync.RWMutex
+	data map[string]string
+}
+
+func New() *Store {
+	return &Store{
+		data: make(map[string]string),
+	}
+}
+
+func (s *Store) Get(id string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	val, ok := s.data[id]
+	return val, ok
+}
+
+func (s *Store) Put(id string, val string) string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[id] = val
+	return val
+}
+
+func (s *Store) Delete(id string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	_, ok := s.data[id]
+	if !ok {
+		return false
+	}
+	delete(s.data, id)
+	return true
+}
+
+func (s *Store) List() map[string]string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	res := make(map[string]string, len(s.data))
+	for k, v := range s.data {
+		res[k] = v
+	}
+	return res
+}
